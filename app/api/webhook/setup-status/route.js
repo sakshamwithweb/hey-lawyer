0b5db60fe877030e0c87c58b0afbb6d30909e704
sakshamwithweb <websaksham39@gmail.com>
2024-11-30 10:08:02 +0530

    made setup status perfect

--- app/api/webhook/setup-status/route.js
+++ app/api/webhook/setup-status/route.js
@@ -1,11 +1,17 @@
 // app/api/webhook/setup-status/route.js
 import { NextResponse } from 'next/server';
 import logger from '../../../../lib/logger';
+import dbConnect from '@/lib/mongodb';
+import User from '@/models/User';
 
 export async function GET() {
   try {
     logger.info("Checking webhook setup status");
-    // Additional checks can be added here if necessary
+    const { searchParams } = new URL(req.url);
+    const uid = searchParams.get('uid');
+    await dbConnect()
+    const usercheck = await User.findOne({omiUserId:uid})
+    if(!usercheck) return NextResponse.json({is_setup_completed:false}, {status: 400})
     return NextResponse.json(
       {
         is_setup_completed: true,
