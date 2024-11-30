4083ecea490b20b51cecdb822db40c0296772732
sakshamwithweb <websaksham39@gmail.com>
2024-11-30 10:18:25 +0530

    try 3

--- app/api/webhook/setup-status/route.js
+++ app/api/webhook/setup-status/route.js
@@ -4,14 +4,14 @@ import logger from '../../../../lib/logger';
 import dbConnect from '@/lib/mongodb';
 import User from '@/models/User';
 
-export async function GET() {
+export async function GET(req) {
   try {
-    logger.info("Checking webhook setup status");
     const { searchParams } = new URL(req.url);
     const uid = searchParams.get('uid');
-    await dbConnect()
-    const usercheck = await User.findOne({omiUserId:uid})
-    if(!usercheck) return NextResponse.json({is_setup_completed:false}, {status: 400})
+    await dbConnect();
+    const usercheck = await User.findOne({ omiUserId: uid });
+    if (!usercheck) return NextResponse.json({ is_setup_completed: false }, { status: 400 });
+    logger.info("Checking webhook setup status");
     return NextResponse.json(
       {
         is_setup_completed: true,
